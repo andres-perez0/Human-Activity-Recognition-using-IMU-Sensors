76bb99634630f659cd8fe4712b9d717e7efeccd2
andres-perez0 <ap7552897@gmail.com>
2025-08-03 19:58:54 -0700

    model ran after a few syntactic error; model did poorly however; shown little growth over epochs remaining around 53% test accuracy

--- model.py
+++ model.py
@@ -1,17 +1,23 @@
+# for DP modeling
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
 from torch.utils.data import DataLoader, TensorDataset
 from sklearn.model_selection import train_test_split
 
+# for number - crunching
 import numpy as np
-
 import scipy.stats as stats
 
+# for dataset management
 import pandas as pd
 
+# for data visualization
 import matplotlib.pyplot as plt
+import matplotlib_inline.backend_inline
 import seaborn as sns
 
-import pandas as pd
+# for file mangement
 import glob
 import os
 
@@ -87,7 +93,93 @@ batchsize = 64
 train_loader = DataLoader(train_data, batch_size=batchsize,shuffle=True,drop_last=True)
 test_loader = DataLoader(test_data, batch_size=test_data.tensors[0].shape[0])
 
-# # Check sizes of data batches 
-# for x, y in train_loader:
-#     # Remeber to set drop_last=True
-#     print(x.shape, y.shape)
+'''
+# Check sizes of data batches 
+for x, y in train_loader:
+    # Remeber to set drop_last=True
+    print(x.shape, y.shape)
+'''
+
+class HARModel(nn.Module):
+    # New class inherited from the nn.module
+    def __init__(self):
+        super().__init__() # `super()` allows python to access function from nn.module (parent class)
+        # input layer
+        self.input = nn.Linear(6,16)
+
+        # hidden layer
+        self.hidden1=nn.Linear(16, 32)
+
+        # output layer
+        self.output = nn.Linear(32,1)
+
+    def forward(self, x):
+        x = F.relu(self.input(x))
+        x = F.relu(self.hidden1(x))
+        return self.output(x)
+
+
+
+numepochs=1000
+
+def trainTheModel():
+    lossfun=nn.BCEWithLogitsLoss()
+    optimizer=torch.optim.Adam(HARnet.parameters(),lr=0.1)
+
+    # initialize loss
+    losses=torch.zeros(numepochs)
+    trainAcc=[]
+    testAcc=[]
+
+    # loop over epochs
+    for epochi in range(numepochs):
+        HARnet.train()  # sets the model to training mode
+
+        # loop over training data batches
+        batchAcc=[]
+        batchLoss=[]
+
+        for x, y in train_loader:
+            # forward pass and losses
+            yHat = HARnet(x)
+            loss = lossfun(yHat,y)
+
+            # back prop
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            # losses from this batch
+            batchLoss.append(loss.item())
+
+            # compute training accuracy for this batch
+            batchAcc.append( 100*torch.mean(((yHat>0) == y).float()).item() )
+        
+        # now that we've trained through the batches, get their average training accuracy
+        trainAcc.append( np.mean(batchAcc) )
+        # end of batch loop
+        losses[epochi] = np.mean(batchLoss)
+
+        # test accuracy
+        HARnet.eval()
+        x_1,y = next(iter(test_loader)) # extract X, y from test dataloader
+        with torch.no_grad(): # deactivates auto grad
+            yHat=HARnet(x_1)
+
+        testAcc.append( 100*torch.mean(((yHat>0) == y).float()).item())
+
+    # function output
+    return trainAcc, testAcc, losses
+
+HARnet=HARModel()
+trainAcc,testAcc,losses = trainTheModel()
+# print(losses)
+
+fig, ax = plt.subplots(1,2,figsize=(6,3))
+
+ax[0].plot(trainAcc)
+ax[1].plot(testAcc)
+
+plt.show()
+
+        
